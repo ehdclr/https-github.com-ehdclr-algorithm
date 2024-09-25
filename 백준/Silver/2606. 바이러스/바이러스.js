23f13518928bf5db9ed00d151f5b03b8c16f9bae
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-09-25 20:17:46 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9436 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,37 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-// 인접 리스트로 표현
-const n = Number(input[0]);  // 컴퓨터 수
-const k = Number(input[1]);  // 연결 수
+const n = Number(input[0]);
+const links = Number(input[1]);
 
-// 연결 리스트(인접 리스트) 초기화
-const links = Array.from({ length: n + 1 }, () => []);
+let arr = Array.from({length : n+1}, ()=>[])
 
-// 연결 정보 입력
-for (let i = 2; i < 2 + k; i++) {
-    let [a, b] = input[i].split(" ").map(Number);
-    links[a].push(b);
-    links[b].push(a);
+for(let i = 0 ; i< links ;i++){
+  let [a,b] = input[i+2].split(" ").map(Number);
+  arr[a].push(b);
+  arr[b].push(a);
 }
 
-let virus = new Array(n + 1).fill(false); // 방문 여부 확인 배열
-let cnt = 0;  // 감염된 컴퓨터 수
+let visited = new Array(101).fill(false);
 
-// DFS 함수 정의
-function dfs(i) {
-    virus[i] = true; // 현재 노드를 방문 처리
-    cnt++;  // 방문한 노드 카운트
+let cnt = 0;
+function dfs(i){
+  visited[i] = true;
+  cnt++
 
-    // 현재 노드에 연결된 노드들 탐색
-    for (let x of links[i]) {
-        if (!virus[x]) {
-            dfs(x);  // 방문하지 않은 노드에 대해 DFS 재귀 호출
-        }
+  for(let x of arr[i]){
+    if(!visited[x]){
+      dfs(x);
     }
+  }
 }
 
-// 1번 컴퓨터부터 시작
-dfs(1);
-
-console.log(cnt - 1);  // 1번 컴퓨터 제외한 감염된 컴퓨터 수 출력
+dfs(1)
+console.log(cnt-1);
