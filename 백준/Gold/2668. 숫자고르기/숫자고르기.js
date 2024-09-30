0c0308523a9aa490da30d961a1e282944555b860
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-09-30 22:31:40 +0900

    [Gold V] Title: 숫자고르기, Time: 92 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.js
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.js
@@ -1,38 +1,40 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-//숫자를 뽑고 , 뽑힌 숫자와 뽑은 숫자의 집합이 일치해야함
 const n = Number(input[0]);
-let graph = [0];
-for (let i = 1; i <= n; i++) {
-  graph.push(Number(input[i]));
-}
+const arr = [0];
+
+for (let i = 1; i <= n; i++) arr.push(Number(input[i]));
+
+//방향 그래프에서 순환을 말하는거같음
 
 let visited = new Array(n + 1).fill(false);
 let finished = new Array(n + 1).fill(false);
 let result = [];
+let answer = "";
 
 for (let x = 1; x <= n; x++) {
-  if (!visited[x]) dfs(x, graph, visited, finished, result);
+  if (!visited[x]) dfs(x);
 }
 
-console.log(result.length);
-result.sort((a, b) => a - b);
-for (let x of result) console.log(x);
+function dfs(cur) {
+  visited[cur] = true;
+  let nxt = arr[cur];
 
-function dfs(x, graph, visited, finished, result) {
-  visited[x] = true;
-  let y = graph[x];
-  if (!visited[y]) {
-    dfs(y, graph, visited, finished, result);
-  } else if (!finished[y]) {
-    while (y != x) {
-      result.push(y);
-      y = graph[y];
+  if (!visited[nxt]) {
+    dfs(nxt);
+  } else if (!finished[nxt]) {
+    //순환
+    while (nxt != cur) {
+      result.push(nxt);
+      nxt = arr[nxt];
     }
-
-    result.push(x);
+    result.push(cur);
   }
-
-  finished[x] = true;
+  finished[cur] = true;
 }
+
+answer += result.length + "\n";
+result.sort((a,b)=>a-b).map((el)=> answer += el +"\n");
+
+console.log(answer);
