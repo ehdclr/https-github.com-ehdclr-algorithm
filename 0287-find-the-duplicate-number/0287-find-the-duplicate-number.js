8fc598cefe3e9a3729619458d6cf7743b9faaaf8
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-11-14 20:39:31 +0900

    Time: 6 ms (86.07%), Space: 60.8 MB (43.32%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -3,16 +3,22 @@
  * @return {number}
  */
 var findDuplicate = function(nums) {
-    let obj = {};
+    //TODO 공간복잡도 O(1)로 푸는 방법
+    //이미 있는 배열로 푸는 방법
+    let result = Infinity;
 
-    let result = 0;
+    //해당 숫자가 인덱스에 있으면, - 처리 
     nums.map((el)=>{
-        if(!obj[el]) obj[el] =1;
-        else{
-            result = el;
+        let num = Math.abs(el); //숫자 꺼내기 절대값... -인 순간에는 이미 있는 숫자 -> 이숫자의 인덱스에 판단
+        if(nums[num -1] > 0){ //
+            //TODO 양수이면 방문한거라 생각하고
+            nums[num -1] = -nums[num -1];
+        } 
+        else if(nums[num -1] < 0) {
+            //TODO 이미 방문한 곳이라면 
+            result = num;
+            
         }
     })
-
-
     return result;
 };
