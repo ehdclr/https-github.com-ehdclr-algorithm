d97cd52a47f54ed3c504bc1780c68ce16e02424b
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-11-15 15:01:08 +0900

    Time: 16 ms (85.29%), Space: 50.5 MB (73.49%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.js
+++ 0259-3sum-smaller/0259-3sum-smaller.js
@@ -3,31 +3,28 @@
  * @param {number} target
  * @return {number}
  */
-var threeSumSmaller = function(nums, target) {
-    if (nums.length < 3) return 0;
-
-    nums.sort((a, b) => a - b); // 정렬
-    let count = 0; // 결과 카운트
-
-    for (let i = 0; i < nums.length - 2; i++) {
-        // i를 기준으로 투 포인터 실행
-        let j = i + 1;
+ var threeSumSmaller = function(nums, target) {
+    if(nums.length < 3) return 0;
+    //target보다 작은 수들을 
+    nums.sort((a,b) => a-b)
+    let result = 0;
+    for(let i = 0 ; i <nums.length -2;i++){
+        //기준이 되는 기준점
+        let j = i+1;
         let k = nums.length - 1;
-
-        while (j < k) {
+        while(j < k ){
             let sum = nums[i] + nums[j] + nums[k];
-
-            if (sum < target) {
-                // 모든 k > j에 대해 sum < target 성립
-                count += (k - j);
-                j++; // j 증가
-            } else {
-                k--; // 합이 크거나 같으면 k 감소
+            if(sum < target){
+               result += k -j
+               j++;
+            }
+            else{
+                k--
             }
         }
     }
 
-    return count;
+    return result;
 };
 
-console.log(threeSumSmaller([-2, 0, 1, 3], 2)); // 출력: 2
+console.log(threeSumSmaller([-2,0,1,3],2))
