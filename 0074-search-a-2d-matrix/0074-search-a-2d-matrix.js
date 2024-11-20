5863d09ea9c935d47cc0859947f0ee0fcc6dc90c
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-11-20 20:26:47 +0900

    Time: 0 ms (100%), Space: 49.2 MB (34.67%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,19 +4,25 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let n = matrix.length; // 행 갯수 3
-    let m = matrix[0].length; // 열 갯수 4
+    let n = matrix.length; // 가로 축 
+    let m = matrix[0].length; // 세로 축
 
-    let start = 0, end = n * m -1 ;
+    let start = 0;
+    let end = n * m -1;
+
     while(start<=end){
         let mid = parseInt((start+end)/2);
-        let row = parseInt(mid/m)
+        let row = parseInt(mid / m);
         let col = mid % m;
 
-        if(target > matrix[row][col]) start = mid +1;
-        else if (target < matrix[row][col]) end = mid -1;
-        else return true;
-    }
+        if(target > matrix[row][col]){
+            start = mid +1
+        } else if (target < matrix[row][col]){
+            end = mid -1
+        } else{
+            return true
+        }
 
-    return false;
+    }
+    return false
 };
