7dc621744745eb6554d87754748b4cfb6997a978
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-11-27 21:42:54 +0900

    Time: 0 ms (100%), Space: 48.6 MB (91.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,24 +3,6 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let leftIndex = 0; //0만 저장하는 포인터
-    let rightIndex = nums.length -1; //2만저장하는 포인터
-
-    let curIndex = 0 ; //현재 인덱스
-    while(curIndex <= rightIndex){
-        if(nums[curIndex] == 0){
-            [nums[leftIndex], nums[curIndex]] = [nums[curIndex],nums[leftIndex]];
-            leftIndex++;
-            curIndex++
-        } else if(nums[curIndex] == 2){
-            [nums[rightIndex], nums[curIndex]] = [nums[curIndex], nums[rightIndex]];
-            rightIndex--
-        }
-        else {
-                curIndex++;
-        }
-
-    }
-
-    return nums;
+    //O(nlogn)으로 푸는 단순한 방법은 정렬
+    return nums.sort();
 };
