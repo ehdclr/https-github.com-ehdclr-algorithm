801460ef3750f63f2bdce8628b5e06f815da571a
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-11-28 23:54:13 +0900

    Time: 0 ms (100%), Space: 49 MB (37.91%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -1,21 +1,53 @@
+//TODO 스택 하나로만 푸는방법 
+// const decodeString = s => {
+//   const stack = [];
+//   for (const char of s) {
+//     if (char !== "]") { stack.push(char); continue; }
+//     let cur = stack.pop();
+//     let str = '';
+//     while (cur !== '[') {
+//       str = cur + str;
+//       cur = stack.pop();
+//     }
+//     let num = '';
+//     cur = stack.pop();
+//     while (!Number.isNaN(Number(cur))) {
+//       num = cur + num;
+//       cur = stack.pop();
+//     }
+//     stack.push(cur);
+//     stack.push(str.repeat(Number(num)));
+//   }
+//   return stack.join('');
+// };
+
+//TODO 공간복잡도를 늘리고 스택 두개로 푸는방법
 const decodeString = s => {
-  const stack = [];
-  for (const char of s) {
-    if (char !== "]") { stack.push(char); continue; }
-    let cur = stack.pop();
-    let str = '';
-    while (cur !== '[') {
-      str = cur + str;
-      cur = stack.pop();
+    let stack = [];
+    let num_stack = [];
+
+    let curNum = 0;
+    let curStr = '';
+
+    for(let char of s){
+        if (char == "[") {
+            stack.push(curStr);
+            num_stack.push(curNum);;
+            curNum =0;
+            curStr =''
+            continue;
+        } 
+        else if(char == "]"){
+            let prevStr = stack.pop();
+            let num = num_stack.pop();
+            curStr = prevStr + curStr.repeat(num);
+            continue;
+        } else if(Number.isInteger(+char)){
+            curNum = curNum*10 + Number(char);
+        } else {
+            curStr += char;
+        }
     }
-    let num = '';
-    cur = stack.pop();
-    while (!Number.isNaN(Number(cur))) {
-      num = cur + num;
-      cur = stack.pop();
-    }
-    stack.push(cur);
-    stack.push(str.repeat(Number(num)));
-  }
-  return stack.join('');
-};
+
+    return curStr;
+}
