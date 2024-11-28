e541d83f00c9da1e4f6ca4f7e28b09e8cc77c248
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-11-28 22:29:50 +0900

    Time: 7 ms (100%), Space: 57.4 MB (83%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.js
@@ -3,39 +3,39 @@
  * @param {number} k
  * @return {string}
  */
- var removeDuplicates = function(s, k) {
-    //두개의 스택을 만든다
-    let stack =[];
-    let countStack = []; //반복되는 top의 갯수를 세는 스택
+var removeDuplicates = function(s, k) {
 
-    for(let char of s){
-        if(stack.length ==0 ){
+    let stack = [];
+    let countStack = []; // 해당 같은 문자열의 갯수를 셀 count Stack을 추가
+    //만약 top 이 k와 같아진다면 해당 k만큼 stack에서 빼야함(pop)
+
+    for(let char of s) {
+        if (stack.length == 0 ){
             stack.push(char);
             countStack.push(1);
         } else {
             let top = stack[stack.length -1];
-            if(char == top){
-                // char와 top 이 같을 때, countStack의 top이 k -1 와 같다면 (넣지 않아야하니까)
+            if (top == char && !!top){
+                //여기서 두가지경우 k와 같을 때임 
                 let countTop = countStack[countStack.length -1];
-                //만약에 같다면, stack에서 pop을 k-1 만큼 하면됨
-                if(countTop == k - 1 ){
-                    for(let i = 1; i <= k -1 ; i++ ){
+                if(countTop == k-1){
+                    for(let i = 1 ; i <= k - 1;i++){
                         stack.pop()
                     }
                     countStack.pop();
                 } else {
-                    //만약에 다르다면, stack에 push하고 countTop을 업데이트
-                    stack.push(char);
-                    countStack[countStack.length - 1] = countTop + 1;
+                  stack.push(char);
+                  countStack[countStack.length -1] = countTop +1;
                 }
-            } else {
+            } else { 
+                //다르면 그냥 Push
                 stack.push(char);
                 countStack.push(1);
             }
         }
+        
     }
+
 
     return stack.join("")
 };
-
-console.log(removeDuplicates("deeedbbcccbdaa",3))
