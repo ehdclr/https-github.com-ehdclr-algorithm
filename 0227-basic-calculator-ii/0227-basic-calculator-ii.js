36f2648e16719e9461526543897709df3b30dfb5
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-03 22:45:37 +0900

    Time: 20 ms (90.58%), Space: 58.7 MB (59.14%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.js
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -2,38 +2,128 @@
  * @param {string} s
  * @return {number}
  */
- var calculate = function (s) {
+
+ //TODO 시간복잡도 O(n) , O(n)의 방법
+//  var calculate = function (s) {
+//     let stack = [];
+//     let curNum = 0;
+//     let prevOper = "+"; // 초기 연산자는 '+'로 설정
+
+//     s = s.replace(/\s+/g, ""); 
+
+//     for (let i = 0; i < s.length; i++) {
+//         let char = s[i];
+
+//         // 숫자인 경우, 숫자를 누적
+//         if (Number.isInteger(+char)) {
+//             curNum = curNum * 10 + Number(char);
+//         }
+
+//         // 연산자이거나 마지막 문자인 경우
+//         if (!Number.isInteger(+char) || i === s.length - 1) {
+//             if (prevOper === "+") {
+//                 stack.push(curNum);
+//             } else if (prevOper === "-") {
+//                 stack.push(-curNum);
+//             } else if (prevOper === "*") {
+//                 stack.push(stack.pop() * curNum);
+//             } else if (prevOper === "/") {
+//                 stack.push(Math.trunc(stack.pop() / curNum)); // 정수 나눗셈
+//             }
+//             prevOper = char; // 현재 연산자로 업데이트
+//             curNum = 0; // 현재 숫자 초기화
+//         }
+//     }
+
+//     // 스택의 모든 값을 더한 결과 반환
+//     return stack.reduce((a, b) => a + b, 0);
+// };
+
+//TODO O(n) O(1) 방법
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+ //TODO 시간복잡도 O(n) , O(n)의 방법
+//  var calculate = function (s) {
+//     let stack = [];
+//     let curNum = 0;
+//     let prevOper = "+"; // 초기 연산자는 '+'로 설정
+
+//     s = s.replace(/\s+/g, ""); 
+
+//     for (let i = 0; i < s.length; i++) {
+//         let char = s[i];
+
+//         // 숫자인 경우, 숫자를 누적
+//         if (Number.isInteger(+char)) {
+//             curNum = curNum * 10 + Number(char);
+//         }
+
+//         // 연산자이거나 마지막 문자인 경우
+//         if (!Number.isInteger(+char) || i === s.length - 1) {
+//             if (prevOper === "+") {
+//                 stack.push(curNum);
+//             } else if (prevOper === "-") {
+//                 stack.push(-curNum);
+//             } else if (prevOper === "*") {
+//                 stack.push(stack.pop() * curNum);
+//             } else if (prevOper === "/") {
+//                 stack.push(Math.trunc(stack.pop() / curNum)); // 정수 나눗셈
+//             }
+//             prevOper = char; // 현재 연산자로 업데이트
+//             curNum = 0; // 현재 숫자 초기화
+//         }
+//     }
+
+//     // 스택의 모든 값을 더한 결과 반환
+//     return stack.reduce((a, b) => a + b, 0);
+// };
+
+//TODO O(n) O(1) 방법
+
+var calculate = function(s){
+    //바로 연산을 하는 것 *와 /를 바로 연산하기 
+    s = s.replace(/\s+/g,"");
+    let sum = 0;
+
     let stack = [];
+    
     let curNum = 0;
-    let prevOper = "+"; // 초기 연산자는 '+'로 설정
-
-    // 공백 제거 후 한 문자씩 처리
-    s = s.replace(/\s+/g, ""); 
-
+    let prevOper = "+"
+    
+    
     for (let i = 0; i < s.length; i++) {
         let char = s[i];
 
-        // 숫자인 경우, 숫자를 누적
+        // 숫자인 경우
         if (Number.isInteger(+char)) {
-            curNum = curNum * 10 + Number(char);
+            curNum = curNum * 10 + Number(char); // 숫자를 누적
         }
 
         // 연산자이거나 마지막 문자인 경우
         if (!Number.isInteger(+char) || i === s.length - 1) {
             if (prevOper === "+") {
-                stack.push(curNum);
+                stack.push(Number(curNum))
             } else if (prevOper === "-") {
-                stack.push(-curNum);
+                stack.push(-Number(curNum))
             } else if (prevOper === "*") {
                 stack.push(stack.pop() * curNum);
             } else if (prevOper === "/") {
-                stack.push(Math.trunc(stack.pop() / curNum)); // 정수 나눗셈
+                stack.push(Math.trunc(stack.pop() / curNum) )
             }
-            prevOper = char; // 현재 연산자로 업데이트
+
+            // 연산자 업데이트
+            prevOper = char;
             curNum = 0; // 현재 숫자 초기화
         }
     }
 
-    // 스택의 모든 값을 더한 결과 반환
-    return stack.reduce((a, b) => a + b, 0);
-};
+    console.log(stack)
+
+    return stack.reduce((a,b)=> a+b)
+}
+
+console.log(calculate("3+2*2"))
