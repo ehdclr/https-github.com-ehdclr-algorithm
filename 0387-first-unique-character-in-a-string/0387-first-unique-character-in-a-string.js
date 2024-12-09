ddf52849f3ba05f3e24a3a487c918e23c35f708d
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-09 21:26:19 +0900

    Time: 46 ms (19.47%), Space: 55.7 MB (23.01%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -2,27 +2,43 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function(s) {
+
+ //map 사용
+// var firstUniqChar = function(s) {
     
-    let newMap = new Map();
+//     let newMap = new Map();
 
-    //전부 다 돌고
-    for(let idx = 0 ; idx<s.length ; idx++){
-        if(newMap.has(s[idx])){
-            let value = newMap.get(s[idx])
-            newMap.set(s[idx],value + 1)
-        } else {
-            newMap.set(s[idx], 1);
-        }
+//     //전부 다 돌고
+//     for(let idx = 0 ; idx<s.length ; idx++){
+//         if(newMap.has(s[idx])){
+//             let value = newMap.get(s[idx])
+//             newMap.set(s[idx],value + 1)
+//         } else {
+//             newMap.set(s[idx], 1);
+//         }
         
+//     }
+
+//     for(let idx = 0;  idx<s.length ;idx++){
+//         if(newMap.get(s[idx]) ==1){
+//             return idx;
+//         }
+//     }
+
+//     //반복되는 것이 없다면, -1
+//     return -1
+// };
+
+var firstUniqChar = function(s){
+    let mp = {}
+
+    for(let char of s){
+        mp[char] = !mp[char] ? 1 : mp[char] +1;
     }
 
-    for(let idx = 0;  idx<s.length ;idx++){
-        if(newMap.get(s[idx]) ==1){
-            return idx;
-        }
+    for(let i = 0 ; i < s.length ;i++){
+        if(mp[s[i]] == 1) return i;
     }
 
-    //반복되는 것이 없다면, -1
     return -1
-};
+}
