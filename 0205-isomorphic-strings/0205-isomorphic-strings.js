c7e564c141d70e110d168d91e573ce77cdbed729
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-09 21:51:24 +0900

    Time: 12 ms (20.19%), Space: 51.5 MB (37.69%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let mp1 = {};
+    let mp2 = {};
+
+    for(let i = 0 ; i < s.length ;i++){
+        if(!mp1[s[i]]) mp1[s[i]] = t[i]
+        else if(mp1[s[i]] !== t[i]) return false 
+
+        if(!mp2[t[i]]) mp2[t[i]] = s[i]
+        else if(mp2[t[i]] !== s[i]) return false 
+    }
+
+    
+
+    return true
+};
