23209377cd74d285e2833c86e03bdd951c3c1f08
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-10 20:38:14 +0900

    Time: 32 ms (17.15%), Space: 55 MB (10.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,19 +3,37 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    let mp1 = {}
-    let mp2 = {}
 
-    if(s.length != t.length ) return false;
+ //시간복잡도 O(2n) -> O(N) 방법
+// var isAnagram = function(s, t) {
+//     let mp1 = {}
+//     let mp2 = {}
+
+//     if(s.length != t.length ) return false;
+
+//     for(let i = 0 ; i <s.length ;i++){
+//         mp1[s[i]] = (mp1[s[i]] || 0)  + 1;
+//         mp2[t[i]] = (mp2[t[i]] || 0)  + 1;
+//     }
+
+//     for(let i =0 ; i <s.length ;i++){
+//         if(mp1[s[i]] != mp2[s[i]]) return false;
+//     }
+//     return true
+// };
+
+
+//정렬하는 방법 O(nlogn) 방법
+var isAnagram = function(s,t){
+
+    let arr1 = s.split("").sort();
+    let arr2 = t.split("").sort();
+
+    if(arr1.length != arr2.length) return false;
 
     for(let i = 0 ; i <s.length ;i++){
-        mp1[s[i]] = (mp1[s[i]] || 0)  + 1;
-        mp2[t[i]] = (mp2[t[i]] || 0)  + 1;
+        if(arr1[i] != arr2[i]) return false;
     }
 
-    for(let i =0 ; i <s.length ;i++){
-        if(mp1[s[i]] != mp2[s[i]]) return false;
-    }
-    return true
-};
+    return true;
+}
