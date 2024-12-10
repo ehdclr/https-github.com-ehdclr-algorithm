a3197ee67dde03e8f22c591e2a600229d4a1f174
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-10 21:13:22 +0900

    Time: 24 ms (21.98%), Space: 54.2 MB (55.47%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+
+    //arr 로 풀 수도 있음 
+
+    let mp = {};
+    let result = []
+
+    for(let i =0 ; i < nums.length ;i++){
+        mp[nums[i]] = (mp[nums[i]] || 0) + 1;
+    }
+
+    for(let [k,v] of Object.entries(mp)){
+       result.push([Number(k),Number(v)])
+    }
+
+    return result.sort((a,b)=> b[1] - a[1]).slice(0,k).map((el) => el[0]);
+};
