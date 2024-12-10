bf7a3b5d7db019bc612109845610eff5cb1796e3
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-10 20:13:36 +0900

    Time: 10 ms (32.2%), Space: 52 MB (29.13%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.js
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -3,19 +3,33 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
+// var isIsomorphic = function(s, t) {
+//     let mp1 = {};
+//     let mp2 = {};
+
+//     for(let i = 0 ; i < s.length ;i++){
+//         if(!mp1[s[i]]) mp1[s[i]] = t[i]
+//         else if(mp1[s[i]] !== t[i]) return false 
+
+//         if(!mp2[t[i]]) mp2[t[i]] = s[i]
+//         else if(mp2[t[i]] !== s[i]) return false 
+//     }
+
+//     return true
+// };
+
+var isIsomorphic = function(s,t) {
     let mp1 = {};
     let mp2 = {};
 
-    for(let i = 0 ; i < s.length ;i++){
-        if(!mp1[s[i]]) mp1[s[i]] = t[i]
-        else if(mp1[s[i]] !== t[i]) return false 
 
-        if(!mp2[t[i]]) mp2[t[i]] = s[i]
-        else if(mp2[t[i]] !== s[i]) return false 
+    for(let i =0 ; i <s.length ;i++){
+         if(!mp1[s[i]]) mp1[s[i]] = t[i];
+         else if(mp1[s[i]] != t[i]) return false;
+
+         if(!mp2[t[i]]) mp2[t[i]] = s[i]
+         else if(mp2[t[i]] != s[i]) return false;
     }
+    return true;
 
-    
-
-    return true
-};
+}
