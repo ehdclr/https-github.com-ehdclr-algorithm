61c8a23b99fc6131404669e40f3cd313a3cd96b0
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-13 22:30:19 +0900

    Time: 18 ms (42.63%), Space: 56.6 MB (91.29%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.js
+++ 0525-contiguous-array/0525-contiguous-array.js
@@ -1,22 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var findMaxLength = function(nums) {
-    nums.forEach((v, i) => {
-        if (v == 0) nums[i] = -1;
-    });
 
     let sumMap = new Map();
-    let sum = 0;
     let maxLength = 0;
+    let sum = 0;
+    sumMap.set(0,-1);
 
-    sumMap.set(0, -1); // 누적 합이 0인 경우를 처리
-
-    nums.forEach((num, i) => {
+    nums.forEach((v,i) =>{
+        num = v;
+        if(v == 0) num = -1;
         sum += num;
-        if (sumMap.has(sum)) {
+        if(sumMap.has(sum)){
             maxLength = Math.max(maxLength, i - sumMap.get(sum));
-        } else {
-            sumMap.set(sum, i);
+        } else{
+            sumMap.set(sum,i)
         }
-    });
+    })
 
     return maxLength;
 };
