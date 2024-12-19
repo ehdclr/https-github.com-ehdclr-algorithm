ea7f0028be2778fb2986517f10930ceda4bac581
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2024-12-19 23:39:32 +0900

    Time: 0 ms (100%), Space: 51.3 MB (94.48%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -3,23 +3,22 @@
  * @return {number}
  */
 var pivotIndex = function(nums) {
-    //TODO 피봇 왼쪽과 오른쪽 합이 같아야함 그 인덱스를 찾아야함
-    let pivotIndex = 0 ;
-    let prevPivotNum = 0;
+    let pivotIndex = 0 ; //피봇 인덱스를 정하고 ,  피봇 1부터 시작
+    let prevPivotNum = 0; //피봇 이전 숫자 초기화 -> 맨처음에는 아무 숫자도 없으니
 
-    let leftSum = 0;
-    let rightSum = nums.reduce((a,b)=> a+b);
+    let leftSum = 0; //피봇왼쪽 합 초기화
+    let rightSum = nums.reduce((a,b) => a+b)  //오른쪽 합 -> 전체합으로 초기화  
 
 
-    while(pivotIndex < nums.length){
-
+    for( ; pivotIndex < nums.length ;pivotIndex++){
         leftSum += prevPivotNum;
-        rightSum -= nums[pivotIndex]
-        
-        if(leftSum == rightSum ) return pivotIndex
-        prevPivotNum = nums[pivotIndex];
-        pivotIndex ++;
+        rightSum -= nums[pivotIndex];
+
+        if(leftSum == rightSum) return pivotIndex;
+
+        prevPivotNum = nums[pivotIndex]; // 피봇 이전 숫자 업데이트
 
     }
-    return -1
+
+    return -1;
 };
