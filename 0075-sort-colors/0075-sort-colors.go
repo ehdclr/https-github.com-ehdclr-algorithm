1089f34b9d30964b4c5ffd2295993da33dd2d97e
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-04 17:10:35 +0900

    Time: 0 ms (100%), Space: 4.1 MB (8.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.go
+++ 0075-sort-colors/0075-sort-colors.go
@@ -1,20 +1,20 @@
-func sortColors(nums []int) {
-    l, r := 0, len(nums)-1
-    curIndex := 0
+func sortColors(nums []int)  {
+    l, r := 0, len(nums) - 1;
+    curIndex :=0
+
+    //l 이 0을 담을 인덱스 r이 2를 담을 인덱스
 
     for curIndex <= r {
         if nums[curIndex] == 0 {
-            // 0을 왼쪽으로 이동
-            nums[l], nums[curIndex] = nums[curIndex], nums[l]
-            l++
-            curIndex++
+            //0이라면 l이랑 스왑
+            nums[l], nums[curIndex] = nums[curIndex], nums[l];
+            l++;
+            curIndex++;
         } else if nums[curIndex] == 2 {
-            // 2를 오른쪽으로 이동
-            nums[r], nums[curIndex] = nums[curIndex], nums[r]
-            r--
+            nums[r], nums[curIndex] = nums[curIndex], nums[r];
+            r-- //스왑 한 뒤 1이나 0이 될 수도 있으므로 
         } else {
-            // 1은 그대로 두고 진행
-            curIndex++
+            curIndex++; //1은 그냥 넘어가기
         }
     }
 }
