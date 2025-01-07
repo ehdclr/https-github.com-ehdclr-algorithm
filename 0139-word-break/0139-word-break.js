06a7a7cf3c125fbf6b8b7de833d203365dd17a0f
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-07 23:12:14 +0900

    Time: 8 ms (43.1%), Space: 51 MB (72.11%) - LeetHub

--- 0139-word-break/0139-word-break.js
+++ 0139-word-break/0139-word-break.js
@@ -1,38 +1,21 @@
-// var wordBreak = function(s, wordDict) {
-//     let n = s.length;
-//     let dp = new Array(n + 1).fill(false); 
-//     dp[0] = true; // 빈 문자열은 항상 true
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function(s, wordDict) {
+    let dp = new Array(s.length + 1).fill(false); 
+    dp[0] = true;
 
-//     let wordSet = new Set(wordDict); // 빠른 탐색을 위해 Set 사용
+    for(let i = 0 ; i < s.length ;i++){
+        for(let j = i +1 ; j < s.length +1 ; j++){ // 이후 
+            if(!dp[i]) continue;
 
-//     for (let i = 1; i <= n; i++) { // i는 1부터 n까지 검사
-//         for (let j = 0; j < i; j++) { // j는 i 이전까지 검사
-//             if (dp[j] && wordSet.has(s.slice(j, i))) { // j까지 가능하고 j~i가 단어인지 확인
-//                 dp[i] = true;
-//                 break; // 가능한 경우 더 이상 탐색 불필요
-//             }
-//         }
-//     }
-
-//     return dp[n]; // 최종 결과 반환
-// };
-
-//set으로 푸는 방법
-
-var wordBreak = function(s, wordDict){
-    let n = s.length;
-    let dp = new Array(n+1).fill(false);
-    dp[0] = true // 빈문자열은 항상 true
-
-    for(let i = 0 ; i <  n + 1; i++){
-        if(dp[i] === false) continue; // 바로 이전이니까 
-
-        for(let j = i + 1 ; j < n + 1 ; j++ ) { //j는 다음 dp에 담을 그릇 즉 , 다음 사이즈를 바텀 업으로 확인
-            if(wordDict.includes(s.slice(i,j))) dp[j] = true
+            if(dp[i] && wordDict.includes(s.slice(i,j))){
+                dp[j]  = true;
+            }
         }
-
     }
 
-    return dp[n]
-
-}
+    return dp[s.length]
+};
