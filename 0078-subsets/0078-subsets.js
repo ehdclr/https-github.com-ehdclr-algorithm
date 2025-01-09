ee6f328f09ebf51547f782419e42b1bfacdb6153
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-09 23:25:30 +0900

    Time: 0 ms (100%), Space: 51.9 MB (95.58%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let result = [];
+
+    function dfs(index, current) {
+        // 현재 부분집합을 결과에 추가
+        result.push([...current]);
+
+        // 남은 숫자들로 부분집합 생성
+        for (let i = index; i < nums.length; i++) {
+            current.push(nums[i]);  // 숫자 선택
+            dfs(i + 1, current);   // 다음 숫자 처리
+            current.pop();         // 선택 취소
+        }
+    }
+
+    dfs(0, []); // DFS 시작
+    return result;
+};
