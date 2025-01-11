9308f87d78b467216569d16c84db62585e1a5e63
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-11 23:50:15 +0900

    Time: 0 ms (100%), Space: 51.8 MB (96.31%) - LeetHub

--- 0078-subsets/0078-subsets.js
+++ 0078-subsets/0078-subsets.js
@@ -3,21 +3,21 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
+    
     let result = [];
 
-    function dfs(index, current) {
-        // 현재 부분집합을 결과에 추가
-        result.push([...current]);
+    function dfs(index, array){
+        
+        result.push([...array])
 
-        // 남은 숫자들로 부분집합 생성
-        // 방문하고 다음 숫자로 넘어가야하기때문에 index를 +1 함
-        for (let i = index; i < nums.length; i++) {
-            current.push(nums[i]);  // 숫자 선택
-            dfs(i + 1, current);   // 다음 숫자 처리
-            current.pop();         // 선택 취소
+
+        for(let idx = index ; idx < nums.length ;idx++ ){
+            array.push(nums[idx])
+            dfs(idx + 1,array)
+            array.pop()
         }
     }
 
-    dfs(0, []); // DFS 시작
-    return result;
+    dfs(0,[]);
+    return result
 };
