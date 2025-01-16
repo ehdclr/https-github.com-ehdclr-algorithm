9154bfe50a3875db6369940eadf3ac260c3c4678
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-16 12:54:10 +0900

    Time: 263 ms (90.12%), Space: 49.9 MB (61.13%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -1,41 +1,48 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
 var exist = function (board, word) {
     let m = board.length;
     let n = board[0].length;
 
-    let dx = [-1, 1, 0, 0];
-    let dy = [0, 0, -1, 1];
-    let result = false;
-
     let visited = Array.from({ length: m }, () => new Array(n).fill(false));
+    let flag = false;
+
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             if (board[i][j] === word[0]) {
                 visited[i][j] = true;
-                bt(1, visited, i, j);
-                visited[i][j] = false;
+                bt(i, j, visited, 1);
+                visited[i][j] = false
             }
+
         }
     }
 
-    function bt(depth, visited, x, y) {
-        if (result) return;
-
-        if (depth === word.length) {
-            result = true;
+    function bt(x, y, visited, index) {
+        if (flag ) return;
+        if (index == word.length) {
+            
+            flag = true;
             return;
         }
+
 
         for (let i = 0; i < 4; i++) {
             let nx = x + dx[i];
             let ny = y + dy[i];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && board[nx][ny] === word[depth]) {
+            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && board[nx][ny] == word[index]) {
                 visited[nx][ny] = true;
-                bt(depth + 1, visited, nx, ny);
+                bt(nx, ny, visited, index +1)
                 visited[nx][ny] = false;
             }
         }
     }
 
-    return result;
+    return flag;
 };
