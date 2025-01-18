ff1bbf8f3113f1d693b51782abb594f128e7f263
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-18 10:05:44 +0900

    Time: 264 ms (90.06%), Space: 49.5 MB (67.94%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -8,40 +8,41 @@ var exist = function (board, word) {
     let n = board[0].length;
 
     let visited = Array.from({ length: m }, () => new Array(n).fill(false));
+    let dx = [-1,1,0,0]; //x방향으로 이동
+    let dy = [0,0,-1,1]; //y방향으로 이동
     let flag = false;
-
-    const dx = [-1, 1, 0, 0];
-    const dy = [0, 0, -1, 1];
 
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            if (board[i][j] === word[0]) {
+            //만약에 첫 단어가 같을 때, 
+            if (!visited[i][j] && word[0] == board[i][j]) {
                 visited[i][j] = true;
-                bt(i, j, visited, 1);
-                visited[i][j] = false
+                //백트래킹 호출
+                bt(i, j, visited , 1)
+                visited[i][j] = false; //시작점 원상태
             }
 
         }
     }
 
-    function bt(x, y, visited, index) {
-        if (flag ) return;
-        if (index == word.length) {
-            
+    function bt(x, y, visited ,depth) {
+        if(flag) return;
+        if(depth == word.length){
+            //깊이가 길이가 같아지면, 모든 단어가 통과 했단 듯임
             flag = true;
             return;
         }
-
-
-        for (let i = 0; i < 4; i++) {
+        for(let i = 0 ; i < 4 ;i++){
             let nx = x + dx[i];
             let ny = y + dy[i];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && board[nx][ny] == word[index]) {
+            if(nx >= 0 && nx < m && ny >=0 && ny < n && word[depth] == board[nx][ny] && !visited[nx][ny]){
                 visited[nx][ny] = true;
-                bt(nx, ny, visited, index +1)
+                bt(nx,ny,visited,depth+1);
                 visited[nx][ny] = false;
             }
+
         }
+
     }
 
     return flag;
