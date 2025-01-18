c127a33a5372ed027796a21e3a42a067cf0b795c
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-18 12:47:57 +0900

    Time: 3 ms (74%), Space: 51.1 MB (99%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.js
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.js
@@ -3,48 +3,30 @@
  * @return {number}
  */
 var findUnsortedSubarray = function(nums) {
+    let n = nums.length;
+    let startIdx = -1;
+    let endIdx = -1;
+    let maxSeen = -Infinity;
+    let minSeen = Infinity;
 
-    //꺾이는 순간을 찾아서 
-    let indexes = [];
-   
-   //TODO 루프를 돌면서 가장 작은 부분을 찾고, 그 부분에서 작은부분보다 큰부분이 있다면, 확장하는 식
-   //TODO 오른쪽에서 가장 큰부분을 고르고 오른쪽에서 가장 큰부분을 비교해가면서 오른쪽을 확장해 나가는 방식으로 구현하면된다.
-
-    //먼저 왼쪽에서 시작하는 index pointer를 선언
-    let leftNum = Infinity; // 가장 작은걸 찾는 것이기 때문에 
-    let rightNum = -Infinity; // 가장 큰 값을 찾아서 오른쪽으로 함
-
-    //O(n);
-    for(let i = 1 ; i <nums.length ;i++){
-        if(nums[i-1] > nums[i]){
-            leftNum = Math.min(nums[i],leftNum); //가장 작은 부분을 찾으면된다. 
-        }
-    }
-    // O(n)
-    for(let i = nums.length -2; i >= 0 ; i--){
-        if(nums[i] > nums[i+1]){
-            rightNum = Math.max(nums[i],rightNum);
+    // 오른쪽에서 왼쪽으로 최소값 추적
+    for (let i = 0; i < n; i++) {
+        if (nums[i] < maxSeen) {
+            endIdx = i;
+        } else {
+            maxSeen = nums[i];
         }
     }
 
-    let leftIndex = 0; //왼쪽부터 깨지는 부분찾기
-    while(leftIndex <nums.length && nums[leftIndex] <= leftNum){
-        leftIndex++
+    // 왼쪽에서 오른쪽으로 최대값 추적
+    for (let i = n - 1; i >= 0; i--) {
+        if (nums[i] > minSeen) {
+            startIdx = i;
+        } else {
+            minSeen = nums[i];
+        }
     }
 
-    let rightIndex = nums.length -1;
-    while(rightIndex >= 0  && nums[rightIndex] >= rightNum){
-        rightIndex--;
-    }
-
-    //이미 정렬이 된 경우 leftIndex가 rightIndex보다 작지 않아서 0을 반환
-    return leftIndex < rightIndex ? rightIndex - leftIndex + 1 : 0
-   
-
+    // 정렬되지 않은 구간의 길이를 계산
+    return startIdx === -1 ? 0 : endIdx - startIdx + 1;
 };
-
-//TODO 일정하게 늘어나는 구간이 아닌, 
-
-
-// 늘었다가 줄어드는 전체 구간을 구하는것 
-
