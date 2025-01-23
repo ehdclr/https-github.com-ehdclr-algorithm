606c7d3cdaf8806abd19d10b1a8ddc55014d9393
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-23 23:04:29 +0900

    Time: 0 ms (100%), Space: 50.4 MB (65.12%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -2,42 +2,30 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-    
-    let result = [];
+var generateParenthesis = function (n) {
 
-    //백 트래킹 구현
-    function bt(text,openMark, closeMark){
-        if( closeMark.length == n && openMark.length === n ){ // 닫는것과 동시에 총 깊이가 6이여야함  깊이는 0부터 시작하니
-            result.push(text.join(""))
+    let result =[]
+
+    function bt(text, openCount, closeCount) {
+        if (openCount === n && closeCount === n) {
+            result.push(text.join(""));
             return;
         }
 
-        //적다면 openMark넣기
-        if(openMark.length < n ){
+        if (openCount < n) {
             text.push("(");
-            openMark.push("(")
-            bt(text,openMark,closeMark);
-            text.pop();
-            openMark.pop()
-        } 
-
-        if(closeMark.length < openMark.length){
-            text.push(")");
-            closeMark.push(")")
-            bt(text,openMark,closeMark);
-            text.pop();
-            closeMark.pop()
+            bt(text, openCount + 1, closeCount);
+            text.pop()
         }
+
+        if (closeCount < openCount && closeCount < n) {
+            text.push(")");
+            bt(text, openCount, closeCount + 1);
+            text.pop()
+        }
+
     }
+    bt([],0,0)
 
-    bt([],[],[]);
     return result;
-
 };
-
-//N개의 괄호 쌍을 찾는 백트래킹 
-
-// 브랜치는 하나를 하면 닫거나 열기 괄호 여닫는건 스택으로 해야하지 않나? 닫는거 에 대한 N 개인걸 생각하면됨
-//마지막 마치는 조건은 닫는것의 갯수가 N개 일때임
-// 
