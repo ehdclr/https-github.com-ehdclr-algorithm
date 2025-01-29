30f9846fc7e6bf5bab0f1c7a3e1fa65ad808073b
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-01-29 17:38:16 +0900

    Time: 20 ms (48.72%), Space: 50.6 MB (84.62%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.js
+++ 0259-3sum-smaller/0259-3sum-smaller.js
@@ -3,28 +3,32 @@
  * @param {number} target
  * @return {number}
  */
- var threeSumSmaller = function(nums, target) {
-    if(nums.length < 3) return 0;
-    //target보다 작은 수들을 
-    nums.sort((a,b) => a-b)
-    let result = 0;
-    for(let i = 0 ; i <nums.length -2;i++){
-        //기준이 되는 기준점
-        let j = i+1;
-        let k = nums.length - 1;
-        while(j < k ){
-            let sum = nums[i] + nums[j] + nums[k];
-            if(sum < target){
-               result += k -j
-               j++;
-            }
-            else{
-                k--
-            }
-        }
-    }
+var threeSumSmaller = function(nums, target) {
 
+    if(nums.length < 3 ) return 0;
+
+    nums.sort((a,b) => a-b);
+
+    //i 가 가리키는 부분
+
+    let result = 0;
+
+    for(let i = 0 ; i < nums.length - 2 ; i++){
+        // 
+        let left = i + 1; 
+        let right = nums.length - 1;
+        while(left < right){
+            let sum = nums[i] + nums[left] + nums[right];
+
+            // target 보다 같거나 작으면
+            if(sum < target){
+                result += (right - left); // right - left 사이의 조합이 유효하므로 
+                left++;
+            } else {
+                right--;
+            }
+        }     
+    }
+    
     return result;
 };
-
-console.log(threeSumSmaller([-2,0,1,3],2))
