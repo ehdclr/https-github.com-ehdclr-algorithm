ce05c8168e7c5d84aee76e74bd328092dd3388f4
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-03 22:11:23 +0900

    Time: 60 ms (19.48%), Space: 73.3 MB (85.71%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.js
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.js
@@ -4,35 +4,34 @@
  */
 
 function isPalindrome(s) {
-    let left = 0, right = s.length - 1;
-    while (left < right) {
-        if (s[left] !== s[right]) return false;
-        left++;
-        right--;
-    }
-    return true;
+    return s.split("").reverse().join("") == s;
 }
 
-function backtrack(s, start, path, result) {
-    if (start === s.length) {
+
+function bt(s, start, path, result){
+    if(start === s.length){  //앞에서 자르는 간격을 늘릴 것 
         result.push([...path]);
         return;
     }
-    
-    for (let end = start; end < s.length; end++) {
-        let substring = s.substring(start, end + 1);
-        if (isPalindrome(substring)) {
-            path.push(substring);
-            backtrack(s, end + 1, path, result);
-            path.pop();
+
+    for(let end = start ; end < s.length ; end++){
+        let subString = s.substring(start, end +1); // a|ab aa|b 이런식으로 앞에서부터 자르는걸로
+        if(isPalindrome(subString)){
+            path.push(subString);
+            bt(s, end + 1, path, result);
+            path.pop()
         }
     }
+
 }
 
 var partition = function (s) {
-    let result = [];
-    backtrack(s, 0, [], result);
+    if (s.length === 0) return [];
+
+    let result = [];// 결과를 담을 것
+
+    bt(s, 0, [], result);
+
+
     return result;
 };
-
-console.log(partition("aab"));
