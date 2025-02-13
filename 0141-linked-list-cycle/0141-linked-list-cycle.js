9678d3d8e302e9a112c32ed95f39dcc5a2c040ab
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-13 21:47:53 +0900

    Time: 62 ms (70.35%), Space: 52.8 MB (78.45%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -12,20 +12,34 @@
  */
 
  //공간 O(n) 시간복잡도 O(n)
-var hasCycle = function(head) {
+// var hasCycle = function(head) {
 
-    if( head == null) return false;
+//     if( head == null) return false;
 
-    let nodeSet = new Set();
-    let currentNode = head;
+//     let nodeSet = new Set();
+//     let currentNode = head;
 
-    while(currentNode){
-        if(nodeSet.has(currentNode)) return true;
-        nodeSet.add(currentNode);
-        currentNode = currentNode.next
-    }
-    return false;
+//     while(currentNode){
+//         if(nodeSet.has(currentNode)) return true;
+//         nodeSet.add(currentNode);
+//         currentNode = currentNode.next
+//     }
+//     return false;
     
-};
+// };
 
 //O(1) 공간복잡도를 만들어달라 -> fast , slow 방법 fast와 SLOW가 만나게 되면 TRUE 시간 복잡도 O(N) + O(K)
+var hasCycle = function(head) {
+    if (head == null) return false;
+
+    let fast = head, slow = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (fast === slow) return true; // 두 포인터가 만나면 순환 존재
+    }
+
+    return false;
+};
