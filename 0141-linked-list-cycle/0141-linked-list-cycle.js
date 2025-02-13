4b5a95144a0988d5220932064cce304bbdb09b41
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-13 21:45:52 +0900

    Time: 44 ms (99.85%), Space: 54.4 MB (14.48%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,31 @@
+
+
+  function ListNode(val) {
+      this.val = val;
+      this.next = null;
+  }
+
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+ //공간 O(n) 시간복잡도 O(n)
+var hasCycle = function(head) {
+
+    if( head == null) return false;
+
+    let nodeSet = new Set();
+    let currentNode = head;
+
+    while(currentNode){
+        if(nodeSet.has(currentNode)) return true;
+        nodeSet.add(currentNode);
+        currentNode = currentNode.next
+    }
+    return false;
+    
+};
+
+//O(1) 공간복잡도를 만들어달라 -> fast , slow 방법 fast와 SLOW가 만나게 되면 TRUE 시간 복잡도 O(N) + O(K)
