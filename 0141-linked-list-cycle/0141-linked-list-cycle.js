7ec6a9a90a1a4789c9be15749f59b632a109fcad
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-14 21:55:35 +0900

    Time: 63 ms (65.97%), Space: 53.5 MB (31.93%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -1,45 +1,25 @@
-
-
-  function ListNode(val) {
-      this.val = val;
-      this.next = null;
-  }
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
 
 /**
  * @param {ListNode} head
  * @return {boolean}
  */
-
- //공간 O(n) 시간복잡도 O(n)
-// var hasCycle = function(head) {
-
-//     if( head == null) return false;
-
-//     let nodeSet = new Set();
-//     let currentNode = head;
-
-//     while(currentNode){
-//         if(nodeSet.has(currentNode)) return true;
-//         nodeSet.add(currentNode);
-//         currentNode = currentNode.next
-//     }
-//     return false;
-    
-// };
-
-//O(1) 공간복잡도를 만들어달라 -> fast , slow 방법 fast와 SLOW가 만나게 되면 TRUE 시간 복잡도 O(N) + O(K)
 var hasCycle = function(head) {
-    if (head == null) return false;
+    let currentNode = head;
+    let newSet = new Set();
 
-    let fast = head, slow = head;
+    while(currentNode){
+        if(newSet.has(currentNode)) return true
+        else newSet.add(currentNode);
 
-    while (fast && fast.next) {
-        slow = slow.next;
-        fast = fast.next.next;
-
-        if (fast === slow) return true; // 두 포인터가 만나면 순환 존재
-    }
+        currentNode = currentNode.next;
+    }   
 
     return false;
 };
