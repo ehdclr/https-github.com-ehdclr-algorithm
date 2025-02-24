463382a377e1a846da125da18e79af71c93965a6
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-24 23:06:14 +0900

    Time: 0 ms (100%), Space: 57.8 MB (5.24%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+ //ITERATIVE 방법 
+var reverseList = function(head) {
+    if (!head) return head;
+    else if (!head.next) return head;
+
+    let currentNode = head.next;
+    let prevNode = head;
+    head.next = null;
+
+    while(currentNode){
+        let tmpNxtNode = currentNode.next;
+        currentNode.next = prevNode; 
+        prevNode = currentNode;
+        currentNode = tmpNxtNode;
+    }
+
+    return prevNode;
+};
