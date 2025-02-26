174851da4044c2ccea5c789791e7ae4d1c12331f
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-26 21:10:16 +0900

    Time: 4 ms (77.11%), Space: 61.5 MB (9.24%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
@@ -0,0 +1,45 @@
+
+
+  function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+  }
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    // 스택으로 역방향
+    let stack1 = [], stack2 =[];
+
+    while(l1 !== null){
+        stack1.push(l1.val);
+        l1 = l1.next;
+    }
+
+    while(l2 !== null){
+        stack2.push(l2.val)
+        l2 = l2.next;
+    }
+
+    let carry = 0;
+    let head = null;
+
+    while(stack1.length > 0 || stack2.length > 0 || carry !==0 ){
+        let sum = carry;
+
+        if(stack1.length > 0) sum += stack1.pop();
+        if(stack2.length > 0) sum += stack2.pop();
+
+        carry = Math.floor(sum / 10);
+        let newNode = new ListNode(sum % 10);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    return head;
+
+};
