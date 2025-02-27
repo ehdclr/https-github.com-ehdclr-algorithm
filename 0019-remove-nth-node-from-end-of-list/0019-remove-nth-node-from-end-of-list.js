48494df40094c3a09ab13ae2eee7ba82740aeda9
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-27 23:06:19 +0900

    Time: 0 ms (100%), Space: 54.2 MB (7.58%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -10,30 +10,59 @@
  * @param {number} n
  * @return {ListNode}
  */
-var removeNthFromEnd = function(head, n) {
-    if(n==0) return head;
+// var removeNthFromEnd = function(head, n) {
+//     if(n==0) return head;
 
-    let count = 0; 
-    let current = head;
+//     let count = 0; 
+//     let current = head;
     
-    while(current !== null){
-        count ++
-        current = current.next;
-    }
+//     while(current !== null){
+//         count ++
+//         current = current.next;
+//     }
 
-    let countToGo = 0
+//     let countToGo = 0
+//     let dummy = new ListNode(0);
+//     dummy.next = head;
+//     current = dummy;
+    
+//     while(current !== null && current.next !== null){
+//         if(countToGo == count - n){
+//             current.next = current.next.next;
+//         }
+//         countToGo++
+//         current = current.next
+//     }
+
+
+//     return dummy.next;
+// };
+
+
+// twoPointers 방법
+var removeNthFromEnd = function(head, n){
+    if(n == 0) return head;
+
     let dummy = new ListNode(0);
     dummy.next = head;
-    current = dummy;
-    
-    while(current !== null && current.next !== null){
-        if(countToGo == count - n){
-            current.next = current.next.next;
-        }
-        countToGo++
-        current = current.next
+    let firstNode = dummy;
+
+    //두개를 먼저 출발해두면, SECOND가 뒤늦게 출발해서 빠지게됨
+    for(let i = 0 ; i <= n ; i++){
+        firstNode = firstNode.next;
     }
 
+    let secondNode = dummy;
+    while(firstNode !== null){
+        firstNode = firstNode.next;
+        secondNode = secondNode.next;
+    }
 
-    return dummy.next;
-};
+    secondNode.next = secondNode.next.next
+    return dummy.next
+
+}
+
+// var removeNthFromEnd = function(head, n){
+    
+// }
