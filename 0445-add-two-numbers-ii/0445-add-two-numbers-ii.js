98e51c6793843ad61a7a7ac12e4b93fd21b45238
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-02-27 22:17:37 +0900

    Time: 3 ms (88.93%), Space: 61.4 MB (11.07%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.js
@@ -1,18 +1,17 @@
-
-
-  function ListNode(val, next) {
-     this.val = (val===undefined ? 0 : val)
-     this.next = (next===undefined ? null : next)
-  }
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    // 스택으로 역방향
-    let stack1 = [], stack2 =[];
+    let stack1 = [], stack2 = [];
 
     while(l1 !== null){
         stack1.push(l1.val);
@@ -20,26 +19,27 @@ var addTwoNumbers = function(l1, l2) {
     }
 
     while(l2 !== null){
-        stack2.push(l2.val)
+        stack2.push(l2.val);
         l2 = l2.next;
     }
 
-    let carry = 0;
-    let head = null;
+    let carry = 0; //자리 수 올라간다면,
+    let head = null; //맨끝의 더미 헤드 
 
-    while(stack1.length > 0 || stack2.length > 0 || carry !==0 ){
-        let sum = carry;
+    while(stack1.length > 0 || stack2.length > 0 || carry !== 0){
+        let sum = carry; //맨처음엔 더미 부분이니까
 
-        if(stack1.length > 0) sum += stack1.pop();
+        if(stack1.length > 0) sum += stack1.pop()
         if(stack2.length > 0) sum += stack2.pop();
 
-        carry = Math.floor(sum / 10);
+
+        carry = Math.floor(sum / 10)
         let newNode = new ListNode(sum % 10);
 
         newNode.next = head;
         head = newNode;
+
     }
 
     return head;
-
 };
