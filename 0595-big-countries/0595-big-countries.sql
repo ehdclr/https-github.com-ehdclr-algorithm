7804a251e2924dc01404d750f4bbaa61751afe28
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-03-12 21:07:31 +0900

    Time: 620 ms (21.78%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 
 -- 순서는 상관 없다. name population 최소 25000000 area가 최소 3000000 인곳
-select name, population, area from World where population >= 25000000 OR area >= 3000000;
+select name, population, area from World where area >= 3000000 or population >= 25000000
