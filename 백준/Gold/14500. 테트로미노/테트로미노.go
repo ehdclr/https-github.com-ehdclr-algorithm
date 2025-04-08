360baeb4fc55a83d08ff03f16ca9968bb033e009
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-04-08 21:17:44 +0900

    [Gold IV] Title: 테트로미노, Time: 108 ms, Memory: 5736 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.go
+++ 백준/Gold/14500. 테트로미노/테트로미노.go
@@ -9,42 +9,43 @@ import (
 )
 
 var (
-	maxSum  int
-	n, m    int
-	s       = bufio.NewScanner(os.Stdin)
-	w       = bufio.NewWriter(os.Stdout)
+	s      = bufio.NewScanner(os.Stdin)
+	w      = bufio.NewWriter(os.Stdout)
+	n, m   int
+	maxSum int
+
 	visited [][]bool
 	board   [][]int
+	dx      = []int{0, 0, -1, 1}
+	dy      = []int{-1, 1, 0, 0}
 )
 
-type Coordinates struct {
-	x int
-	y int
-}
-
 func main() {
+
 	defer w.Flush()
 	s.Split(bufio.ScanLines)
-	hw := scanInt()
+
+	hw := scanLines()
 	n, m = hw[0], hw[1]
 
-	visited = make([][]bool, n)
 	board = make([][]int, n)
+	visited = make([][]bool, n)
 
 	for i := 0; i < n; i++ {
-		nums := scanInt() // ← 한 줄만 읽음
-		board[i] = nums   // ← 그대로 전체 할당
 		visited[i] = make([]bool, m)
+		line := scanLines()
+		board[i] = line
 	}
 
 	for i := 0; i < n; i++ {
 		for j := 0; j < m; j++ {
-			visited[i][j] = true //시작점도 bt처럼 해줘야함
+			visited[i][j] = true
 			bt(i, j, 1, board[i][j])
 			visited[i][j] = false
 			extraShapes(i, j)
 		}
 	}
+
 	fmt.Fprintln(w, maxSum)
 }
 
@@ -56,59 +57,58 @@ func bt(x, y, depth, sum int) {
 		return
 	}
 
-	dx := []int{0, 0, -1, 1}
-	dy := []int{-1, 1, 0, 0}
-
 	for i := 0; i < 4; i++ {
-		nx := x + dx[i]
-		ny := y + dy[i]
+		nx, ny := x+dx[i], y+dy[i]
 		if nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] {
 			visited[nx][ny] = true
 			bt(nx, ny, depth+1, sum+board[nx][ny])
 			visited[nx][ny] = false
 		}
 	}
-
 }
 
-// 백트래킹으로 못만드는 모양은 처리
 func extraShapes(x, y int) {
+
 	shapes := [][][]int{
-		{{0, 0}, {0, -1}, {0, 1}, {1, 0}},
-		{{0, 0}, {0, -1}, {0, 1}, {-1, 0}},
-		{{0, 0}, {0, -1}, {-1, 0}, {1, 0}},
-		{{0, 0}, {0, 1}, {-1, 0}, {1, 0}},
+		{{0, 0}, {-1, 0}, {1, 0}, {0, -1}},
+		{{0, 0}, {-1, 0}, {1, 0}, {0, 1}},
+		{{0, 0}, {0, 1}, {1, 0}, {0, -1}},
+		{{0, 0}, {0, 1}, {-1, 0}, {0, -1}},
 	}
 
 	for _, shape := range shapes {
 		sum := 0
-		isValid := true
-		for _, dir := range shape {
-			nx, ny := x+dir[0], y+dir[1]
+		founded := true
 
-			if nx >= 0 && ny >= 0 && nx < n && ny < m {
+		for _, v := range shape {
+			nx, ny := x+v[0], y+v[1]
+			if nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] {
 				sum += board[nx][ny]
 			} else {
-				isValid = false
+				founded = false
 				break
 			}
+
 		}
-		if isValid {
+
+		if founded {
 			if maxSum < sum {
 				maxSum = sum
 			}
 		}
+
 	}
+
 }
 
-func scanInt() []int {
+func scanLines() []int {
 	s.Scan()
 	parts := strings.Fields(s.Text())
-	nums := make([]int, len(parts))
+	num := make([]int, len(parts))
 
 	for i, v := range parts {
-		nums[i], _ = strconv.Atoi(v)
+		num[i], _ = strconv.Atoi(v)
 	}
 
-	return nums
+	return num
 }
