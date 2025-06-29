889005a552c858a2d2dd7107c1a86c947badfedf
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-06-29 21:35:19 +0900

    [Bronze II] Title: Strfry, Time: 164 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -1,16 +1,9 @@
+from collections import Counter
 
 N = int(input())
-
 for _ in range(N):
     a, b = input().split()
-    
-    # 문자열 길이가 다르면 불가능
-    if len(a) != len(b):
-        print("Impossible")
-        continue
-    
-    # 정렬해서 비교
-    if sorted(a) == sorted(b):
+    if Counter(a) == Counter(b):
         print("Possible")
     else:
         print("Impossible")
