86fd58228532723b14f94fd94c526325399d039b
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-07-03 10:56:16 +0900

    [Silver II] Title: 스택 수열, Time: 184 ms, Memory: 38876 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,27 +1,26 @@
 import sys
 
 n = int(sys.stdin.readline())
-
 target = [int(sys.stdin.readline()) for _ in range(n)]
 
-stack = []
-operations = []
 curr = 1
+stack = []
+result = []
 
 for num in target:
-    
-    while curr <= num:
-        # 만약에 num 보다 작은경우
-        stack.append(curr)
-        operations.append("+")
-        curr += 1
+  
+  while curr <= num:
+    result.append("+")
+    stack.append(curr)
+    curr += 1
+  # 늘렸고
+  if stack[-1] == num:
+    result.append("-")
+    stack.pop()
+  else:
+    print("NO")
+    exit()
 
-    if stack and stack[-1] == num:
-        stack.pop()
-        operations.append("-")
-    else:
-        print("NO")
-        exit()
+for op in result:
+  print(op)
 
-for op in operations:
-    print(op)
