ac8ac266d7eef261a9add5a1908902a874ba9343
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-07-10 22:23:43 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,32 +1,11 @@
-import sys
 from collections import deque
 
-n = int(sys.stdin.readline())
+n = int(input())
+q = deque(range(1, n+1))
 
-# 제일 위에있는걸 버리고 그다음에 제일 위에있는 카드를 제일 아래에 있는걸 버린다.
+while len(q) > 1:
+    q.popleft()  # 맨 위 카드 버리기
+    if q:  # 카드가 남아있다면
+        q.append(q.popleft())  # 맨 위 카드를 맨 아래로
 
-# 홀수 카운트 경우 버림
-# 짝수 카운트는 뒤에 넣음
-
-q = deque()
-
-for i in range(1, n+1):
-  q.append(i)
-
-count = 1
-
-while len(q) != 1 or count % 2 == 0:
-  # 구현을 하라 할때 , 마지막 하나가 남는데, 
-
-  if count % 2 == 0:
-    q.append(q.popleft())
-  else :
-    q.popleft()
-  
-  count +=1
-
-
-print(q[-1])
-  
-
-
+print(q[0])
