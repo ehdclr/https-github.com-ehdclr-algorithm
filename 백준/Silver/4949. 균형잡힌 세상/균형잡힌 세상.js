b92f10a5134f06b68f89ddf439c589ec85954636
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-07-19 14:30:10 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 168 ms, Memory: 12512 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
@@ -1,29 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync('/dev/stdin').toString().split("\n");
 
-let line = input.slice()
+let line = input.slice();
 let answer = ""
+for (let str of line) {
+  if (str == ".") {
+    break
+  }
 
-for (let i = 0; i < line.length; i++) {
-  let str = line[i];
   let stack = [];
-  if (str == ".") break;
-
 
   for (let char of str) {
     if (char == "(" || char == "[") {
-      stack.push(char)
-    } else if (char == "]" || char == ")") {
-      if (char == ")" && stack[stack.length - 1] == "(") {
+      stack.push(char);
+    } else if (char == ")" || char == "]") {
+      if (stack[stack.length - 1] == "(" && char == ")") {
         stack.pop()
-      } else if (char == "]" && stack[stack.length - 1] == "[") {
+      } else if (stack[stack.length - 1] == "[" && char == "]") {
         stack.pop()
-      } else stack.push(char)
+      } else {
+        stack.push(char)
+      }
     }
   }
 
-  answer += stack.length >= 1 ? "no\n" : "yes\n";
+  answer += stack.length >= 1 ? "no\n" : "yes\n"
 }
 
 console.log(answer)
-
