6fa602ef065ecd4fac93e9895801f892b0549bde
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-09-04 14:19:16 +0900

    Time: 1373 ms (5%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,3 @@
-# Write your MySQL query statement below
-# 최소 5명 이상한테 받는 매니저를 찾으라는거
-
-SELECT e1.name
-FROM Employee e1
-JOIN Employee e2 ON e1.id = e2.managerId
-GROUP BY e1.id
-HAVING COUNT(DISTINCT(e2.id)) >= 5
+select name from employee e inner join
+(select managerId,count(managerId) from employee group by managerId having count(*)>=5) cte
+where e.id=cte.managerId
