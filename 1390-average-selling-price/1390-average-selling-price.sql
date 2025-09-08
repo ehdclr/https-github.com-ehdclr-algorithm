7ceb41e286579b2e5c8e0639ebf097ba762af6dc
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-09-09 08:52:34 +0900

    Time: 1032 ms (12.86%), Space: 0B (100%) - LeetHub

---
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -0,0 +1,10 @@
+SELECT p.product_id, 
+    CASE
+        WHEN SUM(u.units) > 0 THEN ROUND(SUM(p.price * u.units) / SUM(u.units), 2)
+        ELSE 0
+    END as average_price
+FROM Prices p 
+LEFT JOIN UnitsSold u ON p.product_id = u.product_id 
+    AND u.purchase_date >= p.start_date 
+    AND u.purchase_date <= p.end_date
+GROUP BY p.product_id;
