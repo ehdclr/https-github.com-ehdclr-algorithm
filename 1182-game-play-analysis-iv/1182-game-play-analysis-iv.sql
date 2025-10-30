f47a103c7ed53396da71a42b4c754a4141a40428
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-10-31 08:57:08 +0900

    Time: 536 ms (62.09%), Space: 0B (100%) - LeetHub

--- 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/1182-game-play-analysis-iv.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-# Write your MySQL query statement below
 SELECT
   ROUND(COUNT(DISTINCT player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
 FROM
@@ -9,8 +7,3 @@ WHERE
   IN (
     SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id
   )
--- GROUP BY player_id
-
-
-
-# 첫날 로그인하고 재접한 사람 재접률을 말하는듯
