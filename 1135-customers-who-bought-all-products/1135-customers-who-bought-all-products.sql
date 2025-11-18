778e29b9ccf76afc97f0e07f8b65ee81abb18cef
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-11-18 15:45:53 +0900

    Time: 574 ms (33.79%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
-SELECT 
+
+# 모든 product물건을 산 사람을 말하는듯
+select 
     customer_id
-FROM 
-    Customer
-GROUP BY customer_id
-HAVING COUNT(distinct product_key) = (SELECT COUNT(product_key) FROM Product)
+from Customer
+group by customer_id
+having count(distinct(product_key)) = (select count(product_key) from Product)
