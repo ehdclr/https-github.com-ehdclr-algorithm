de38cfda982fe279cd5fdef8cda5951804ee3142
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-12-04 20:43:07 +0900

    [Bronze II] Title: 블랙잭, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,18 +1,24 @@
 const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").trim().split("\n");
-
 const [n, m] = input[0].split(" ").map(Number);
 const nums = input[1].split(" ").map(Number);
+
+nums.sort((a, b) => a - b);
 
 let max = 0;
 
 for (let i = 0; i < n - 2; i++) {
-  for (let j = i + 1; j < n - 1; j++) {
-    for (let k = j + 1; k < n; k++) {
-      const sum = nums[i] + nums[j] + nums[k];
-      if (sum <= m && sum > max) {
-        max = sum;
-      }
+  let left = i + 1;
+  let right = n - 1;
+  
+  while (left < right) {
+    const sum = nums[i] + nums[left] + nums[right];
+    
+    if (sum <= m) {
+      max = Math.max(max, sum);
+      left++;
+    } else {
+      right--;
     }
   }
 }
