ee3491a7dec1ff9826044d195f94f87d21fc0371
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2025-12-06 11:27:38 +0900

    [Bronze I] Title: 이항 계수 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,12 @@
+import sys
+sys.setrecursionlimit(10000)
+
+N,K = map(int,input().split())
+
+
+def bino_coef(n,k):
+  if k == 0 or n == k:
+    return 1
+  return bino_coef(n-1, k) + bino_coef(n-1, k-1)
+
+print(bino_coef(N,K))
