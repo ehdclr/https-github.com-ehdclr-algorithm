6b0ef7bed03af926e405dfd97d850b4279da1c10
Lee HyeonJong <80464000+ehdclr@users.noreply.github.com>
2026-09-11 23:35:54 +0900

    Time: 34 ms (24.13%), Space: 54.5 MB (69.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,19 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map()
-
-    for(let i = 0; i <nums.length ;i++){
-        let complete = target - nums[i];
-
-        if(map.has(complete)){
-            //전체에서 - 해당 숫자를 뺐을 때 이미 있다면, 그 수랑 합쳤을 때 target이 되므로 끝
-            return [map.get(complete),i];
+    for(let i = 0 ; i < nums.length - 1; i ++){
+        for(let j = i+1; j < nums.length ; j++){
+            if(target - nums[j] === nums[i]) return [i,j]
         }
-
-        map.set(nums[i], i);
-
     }
-    return []
-
 };
